fe9e06946072e299ad149aeade1f5d308d8235d3
Victor Alves <victoralvescabral7@gmail.com>
2024-09-23 23:23:12 -0300

    testing phone status

--- app/page.js
+++ app/page.js
@@ -261,7 +261,6 @@ export default function Home() {
                   type="video/mp4"
                 ></source>
               </video>
-              <img className="image__content  opacity-0" alt="bubble-bg" src="bubble-bg.jpg" />
             </div>
           </div>
           <div className="glassy__container"></div>
